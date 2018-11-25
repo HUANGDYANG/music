bd4585801b5815efcaa6e4d0d2834da09bf57d69
HUANGDYANG <1159973635@qq,com>
2018-11-25 16:26:50 +0800

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+// 公共请求参数
+export const commonParams = {
+  g_tk: 1159973635,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0  // 请求正确
---
+++ src/common/js/jsonp.js
@@ -0,0 +1,26 @@
+import originJsonp from 'jsonp'
+
+// 将jsonp库二次封装，返回promise对象
+export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+// 将json格式的data拆分拼接成一串
+export function param(data) {
+  let url = ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
